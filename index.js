bda2b395a00c8e82ce84f10325647c9248eeec5a
Brian Woodward <brian.woodward@gmail.com>
2014-03-23 18:15:19 -0400

    adding expects method and tests

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = function () {
-  return 'Hello, world';
+
+var utils = module.exports = {};
+
+utils.expects = function(config, spec) {
+  for(var k in spec) {
+    if (typeof config[k] === 'undefined') {
+      config[k] = spec[k];
+    }
+  }
 };
--- test/index.js
+++ test/index.js
@@ -1,9 +1,33 @@
+
+var inspect = require('util').inspect;
+
 var expect = require('chai').expect,
-    helpersUtils = require('..');
+    utils = require('..');
 
 describe('helpers-utils', function() {
-  it('should say hello', function(done) {
-    expect(helpersUtils()).to.equal('Hello, world');
+  it('should extend an api', function(done) {
+    var expected = {
+      foo: function () { return 'bar'; },
+      bar: function () {}
+    };
+
+    var spec = {
+      foo: function () {},
+      bar: function () {}
+    };
+
+    var actual = {
+      foo: function () { return 'bar'; }
+    };
+
+    utils.expects(actual, spec);
+
+    expect(actual).to.have.property('foo');
+    expect(typeof actual.foo).to.eql('function');
+
+    expect(actual).to.have.property('bar');
+    expect(typeof actual.bar).to.eql('function');
+
     done();
   });
 });
