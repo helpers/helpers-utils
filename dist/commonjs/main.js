2da0ab5fdadba40d5a392e86426e621be60a36d4
Brian Woodward <brian.woodward@gmail.com>
2014-03-27 15:00:31 -0400

    adding Dates utilities and compiling

--- dist/commonjs/main.js
+++ dist/commonjs/main.js
@@ -1,7 +1,9 @@
 "use strict";
 var Library = require("./helpers-utils/library").Library;
 var Utils = require("./helpers-utils/utils").Utils;
+var Dates = require("./helpers-utils/dates").Dates;
 
 
 exports.Library = Library;
 exports.Utils = Utils;
+exports.Dates = Dates;
